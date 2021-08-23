31b9410380cebddb9f3fc57385894cf761da6ff3
abdulwahab-alobaid-2191115290 <abdulwahab.alobaid@sci.ku.edu.kw>
2021-08-23 18:48:56 +0000

    added python option for the Makefile

--- Makefile_Generator/make.pl
+++ Makefile_Generator/make.pl
@@ -3,8 +3,17 @@
 $ARGC=$#ARGV+1;
 # setting no. of agrs such that the first argument = 1 , instead of 0
 
-if($ARGC != 3){
-	exec('echo "ERROR: invalid no. of arguments , expected <COMPILE MODE> <FILE> <EXE>"');
+# insuring user input
+if($ARGC != 3 && $ARGC != 2){
+	exec('echo "ERROR: invalid no. of arguments , expected <COMPILE MODE> <FILE> <EXE> OR <py> <.py file>"');
+}
+# if user wants the Makefile for python
+elsif($ARGC == 2 && @ARGV[0] eq "py"){	
+	$mode=@ARGV[0];
+	$file=@ARGV[1];
+
+	$command="echo \"all:\n\tpython3 $file\nrm:\n\trm Makefile\">Makefile";
+	exec($command);
 }
 
 $mode=@ARGV[0];		# compile mode , gcc || g++
@@ -12,14 +21,14 @@ $file=@ARGV[1];		# file name , ex. main.c
 $exe=@ARGV[2];		# executable name 
 
 if($mode eq "g++"){
-	$command="echo \"all:\n\tg++ -g -o $exe $file\nrm:\n\trm Makefile\">Makefile";
+	$command="echo \"all:\n\t$mode -g -o $exe $file\nrm:\n\trm Makefile\">Makefile";
 	exec($command);
-	# if mode is g++ set all: target with the debug information and redirect it to the Makefile
+	# if mode is g++ set "all:" target with the debug information and redirect it to the Makefile
 	# did not combine $command & exec because exec does not work with external variables 
 }	
 
 elsif($mode eq "gcc"){
-	$command="echo \"all:\n\tgcc -g -o $exe $file\nrm:\n\trm Makefile\">Makefile";
+	$command="echo \"all:\n\t$mode -g -o $exe $file\nrm:\n\trm Makefile\">Makefile";
 	exec($command);
 }
 else{
